2c3522fb5744a3814c22b8865490a05c0c5a0f42
René K. Støvlbæk <44805521+rene4561@users.noreply.github.com>
2021-05-16 18:05:42 +0200

    Added component eventCard

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,11 +1,28 @@
 <template>
     <div class="event-card">
-
+        <span>@{{ event.time }} on {{ event.date }}</span>
+        <h4>{{ event.title }}</h4>
     </div>
 </template>
 <script>
 export default {
-    name: 'EventCard'
+    name: 'EventCard',
+
+    data() {
+        return {
+            event: {
+                id: 5928101,
+                category: 'animal welfare',
+                title: 'Cat Adoption Day',
+                description: 'Find your new feline friend at this event.',
+                location: 'Meow Town',
+                date: 'January 28, 2022',
+                time: '12:00',
+                petsAllowed: true,
+                organizer: 'Kat Laydee'
+            }
+        }
+    }
 }
 </script>
 <style scoped>
