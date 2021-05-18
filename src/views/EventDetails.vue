7db190a140755cc9c6b3565897669f64c1377042
René K. Støvlbæk <44805521+rene4561@users.noreply.github.com>
2021-05-18 15:54:46 +0200

    Made path to EventDetails

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,28 +1,28 @@
 <template>
-    <div>
-        <h1>{{ event.title }}</h1>
-        <p>{{ event.time }} on {{event.date }} @ {{event.location }}</p>
-        <p>{{ event.desribtion }}</p>
-    </div>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
 </template>
+
 <script>
 import EventService from '@/services/EventService.js'
-
 export default {
-    data() {
-        return {
-            event:null,
-            id: 123
-        }
-    },
-    created() {
-        EventService.getEvent(this.id)
-            .then(response => {
-                this.event = response.data
-            })
-            .catch(error => {
-                console.log(error)
-            })
+  data() {
+    return {
+      event: null,
+      id: 123
     }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 }
 </script>
